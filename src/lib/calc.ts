dbeedfc4b3bb7305ca24a86425da105277aad965
LiamKrenn <k.liam1224@gmail.com>
2024-06-20 21:54:06 +0200

    cleaned up code and huge performance increase to 48k calculations

--- src/lib/calc.ts
+++ src/lib/calc.ts
@@ -1,21 +1,30 @@
 // Using a basic A* algorithm to find the least amount of steps to reach a target number
 
 // Performance test values: 4682 - 15 - ['+', '-', '*', '^']
-// Current performance - Operation Count = 142.9k
+// Current performance - Operation Count = 48k
 
 import { PriorityQueue } from '@datastructures-js/priority-queue';
+
+type QueueItem = {
+	value: number;
+	steps: number;
+	operator: string;
+	stepsList: [number, number, string, number][];
+};
 
 export function leastSteps(target: number, max_src: number, allowedOperators: string[]) {
 	const allowedNumbers = Array.from({ length: max_src }, (_, i) => i + 1).filter(
 		(num) => num !== 10 // 10 can't spawn afaik
 	);
 	const visited = new Set();
-	let queue = new PriorityQueue<{
-		value: number;
-		steps: number;
-		operator: string;
-		stepsList: any[];
-	}>((a, b) => a.steps - b.steps);
+
+	function fValueEvalution(a: QueueItem, b: QueueItem) {
+		// TODO: Optimize
+		return a.steps - b.steps;
+	}
+
+	let queue = new PriorityQueue<QueueItem>(fValueEvalution);
+	queue.enqueue({ value: 0, steps: 0, operator: '', stepsList: [] });
 
 	function applyOperator(a: number, b: number, operator: string) {
 		switch (operator) {
@@ -34,51 +43,58 @@ export function leastSteps(target: number, max_src: number, allowedOperators: st
 		}
 	}
 
+	function skipUnnecessaryOperators(value: number, currentValue: number, operator: string) {
+    // Skip if the value is larger than the target and the operator is +, *, or ^
+		if (
+			(operator === '+' || operator === '*' || operator === '^') &&
+			(currentValue > target || value > target)
+		) {
+			return true;
+		}
+
+    // Skip if the value is smaller than the target and the operator is - or /
+		if ((operator === '-' || operator === '/') && currentValue < target) {
+			return true;
+		}
+
+    // Skip if the value is negative
+		if (value < 0 || currentValue < 0) {
+			return true;
+		}
+
+    // Skip if the value is 1 and the operator is *, /, or ^
+		if (value === 1 && (operator === '*' || operator === '/' || operator === '^')) {
+			return true;
+		}
+	}
+
 	let calc_count = 0;
 
-	queue.enqueue({ value: 0, steps: 0, operator: '', stepsList: [] });
-	console.log('init');
-
 	while (queue.size() > 0) {
-		let queue_el = queue.dequeue();
-		console.log(queue_el);
-
-		if (queue_el.value === target) {
-			console.log('Calculation count: ' + calc_count);
-			return queue_el.stepsList.slice(1);
-		}
+		let element = queue.dequeue();
 		for (let num of allowedNumbers) {
 			for (let operator of allowedOperators) {
-				if (
-					(operator === '+' || operator === '*' || operator === '^') &&
-					(queue_el.value > target || num > target)
-				) {
+        // Skip unnecessary calculations
+				if (skipUnnecessaryOperators(num, element.value, operator)) {
 					continue;
 				}
 
-				if ((operator === '-' || operator === '/') && queue_el.value < target) {
-					continue;
-				}
-
-				if (num < 0 || queue_el.value < 0) {
-					continue;
-				}
-
-				if (num === 1 && (operator === '*' || operator === '/' || operator === '^')) {
-					continue;
-				}
-
-				let newValue = applyOperator(queue_el.value, num, operator) || -1;
+				let newValue = applyOperator(element.value, num, operator) || -1;
 				calc_count++;
 
+        // if the value is way overshooting, skip it (happens quite often with ^)
 				if (newValue > target * 1.2 + max_src || newValue === null || visited.has(newValue)) {
 					continue;
 				}
 				visited.add(newValue);
-				let newStepsList = queue_el.stepsList.concat([[queue_el.value, num, operator, newValue]]);
+				let newStepsList = element.stepsList.concat([[element.value, num, operator, newValue]]);
+				if (newValue === target) {
+					console.log('Calculation count: ' + calc_count);
+					return newStepsList.slice(1);
+				}
 				queue.enqueue({
 					value: newValue,
-					steps: queue_el.steps + 1,
+					steps: element.steps + 1,
 					operator: operator,
 					stepsList: newStepsList
 				});
